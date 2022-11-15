8159a4056e76efaca2ed4184326f0feaf5f94e58
Martin Gullbrandsson <martin.gullbrandson@appliedtechnology.se>
2022-11-15 19:37:44 +0100

    day-2-end

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from 'node-fetch';
+import { MongoClient } from 'mongodb'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const uri = "mongodb+srv://MartinGull:EpiSalt@myawesomecluster.n23pjqb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+const clientPromise = client.connect();
+
+export default async function handler (req, res) {
+
+  const database = (await clientPromise).db('gr8');
+  const collection = database.collection('products');
+  const results = await collection.find({contentful: "7vN8nIqu67q806FfWJNtrj"}).toArray();
+
+  return res.status(200).send(JSON.stringify(results));
 }
